e5233fe0f40f370227c8790ae934bdd9638cda2d
Sahbi <sahbi.g@gmail.com>
2019-12-10 10:39:02 +0300

    Main executable renamed Supply config file as parameter

--- datapipe.py
+++ datapipe.py
@@ -1,4 +1,6 @@
 import logging
+import sys
+
 import yaml
 
 from pipeline import pipeline
@@ -16,9 +18,10 @@ if __name__ == '__main__':
     logging.basicConfig(
         format='%(asctime)-19.19s %(levelname)s:%(name)s:%(lineno)s: %(message)s')
     logging.getLogger(__name__).setLevel(logging.DEBUG)
+    confile = sys.argv[1] if len(sys.argv) > 1 else "config.yaml"
 
     try:
-        with open("config.yaml", 'r') as ymlfile:
+        with open(confile, 'r') as ymlfile:
             cfg = yaml.load(ymlfile, Loader=yaml.Loader)
     except Exception as x:
         logging.error("Failed to read config file " + str(x))
