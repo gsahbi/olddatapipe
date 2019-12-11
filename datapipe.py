683062e32011d765cd3c659bd480214532c8c7f5
Sahbi <sahbi.g@gmail.com>
2019-12-11 08:28:38 +0300

    Fix logging

--- datapipe.py
+++ datapipe.py
@@ -16,8 +16,9 @@ def main(cfg):
 
 if __name__ == '__main__':
     logging.basicConfig(
-        format='%(asctime)-19.19s %(levelname)s:%(name)s:%(lineno)s: %(message)s')
-    logging.getLogger(__name__).setLevel(logging.DEBUG)
+        format='%(asctime)-19.19s %(levelname)s:%(name)s:%(lineno)s: %(message)s',
+        level=logging.DEBUG)
+    #logging.getLogger(__name__).setLevel(logging.DEBUG)
     confile = sys.argv[1] if len(sys.argv) > 1 else "config.yaml"
 
     try:
